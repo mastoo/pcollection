edf3c3251a5aaed9c311dd1db8706da5358b1238
mastoo <a.breglia@gmail.com>
2015-10-26 11:01:06 +0100

    update AsciiTable_copy_paste.cpp

--- AsciiTable_copy_paste.cpp
+++ AsciiTable_copy_paste.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include "AsciiTable.h"
+#include <glm/vec3.hpp> // glm::vec3
+#include <glm/mat4x4.hpp> // glm::mat4
+#include <glm/ext.hpp>
 
 int main(){    using namespace alf;
     
@@ -12,5 +15,14 @@ int main(){    using namespace alf;
     std::cout << tab ;
     
     
+    
+    
+    AsciiTable tab2;    
+    tab2 << "type" << "value" << AsciiTable::endl;    
+    tab2 << "glm::vec3" << glm::to_string(glm::vec3(0.0f, 0.0f,1.0f)) << AsciiTable::endl;
+    tab2 << "glm::mat4" << glm::to_string(glm::perspective(glm::pi<float>() * 0.25f, 4.0f / 3.0f, 0.1f, 100.f)) << AsciiTable::endl;
+    
+    
+    std::cout << tab2;
     return 0;
 }
